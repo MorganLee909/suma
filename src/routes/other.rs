92796f8053239bcbf79d61de2c4a11eb924ac738
Lee Morgan <lee@leemorgan.dev>
2025-07-08 14:40:15 -0400

    Create and serve logos.

--- src/routes/other.rs
+++ src/routes/other.rs
@@ -1,8 +1,12 @@
-use actix_web::{get, HttpResponse, Responder, web};
+use actix_web::{get, HttpResponse, Responder, web, Error};
+use actix_files::NamedFile;
 use crate::HTML;
+use std::path::PathBuf;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(landing_page);
+    cfg.service(svg_logo);
+    cfg.service(png_logo);
 }
 
 #[get("/")]
@@ -11,4 +15,16 @@ async fn landing_page() -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html.clone())
+}
+
+#[get("/logo.svg")]
+async fn svg_logo() -> Result<NamedFile, Error> {
+    let path: PathBuf = PathBuf::from("src/views/logo.svg");
+    Ok(NamedFile::open(path)?)
+}
+
+#[get("/logo.png")]
+async fn png_logo() -> Result<NamedFile, Error> {
+    let path: PathBuf = PathBuf::from("src/views/logo.png");
+    Ok(NamedFile::open(path)?)
 }
