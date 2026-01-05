cdda9bfb6e3ef6bff64a74379a43c5334f6ddf7a
Lee Morgan <lee@leemorgan.dev>
2026-01-05 16:05:05 -0500

    Maybe fixed the issue with switches. Can't test on a phone.

--- src/views/createHtml.js
+++ src/views/createHtml.js
@@ -4,7 +4,7 @@ import htmlMinifier from "html-minifier-terser";
 import fsSync from "fs";
 
 const fs = fsSync.promises;
-const isProduction = false;
+const isProduction = true;
 
 const esbuildProm = esbuild.build({
     entryPoints: [`${import.meta.dirname}/js/index.js`, `${import.meta.dirname}/css/index.css`],
